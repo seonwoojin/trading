589c55b047592a23d64bd1faf2713548d277bdfa
Seonwoo <wlstjsdn12@gmail.com>
2022-10-18 19:21:25 +0900

    .

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const production = "https://www.seonwoomoney.com";
+const production = "https://seonwoomoney.com";
 const development = "http://localhost:4000/";
 const url = process.env.NODE_ENV !== "development" ? production : development;
 
--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ import api from "./api";
 const app = new Koa();
 const router = new Router();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
