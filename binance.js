d46fc4d5e3a5ddc2ec692e246b7eccb001a6d73d
Seonwoo <wlstjsdn12@gmail.com>
2022-11-18 00:40:12 +0900

    Update binance.js

--- binance.js
+++ binance.js
@@ -1182,7 +1182,7 @@ async function home(coin) {
       bbfix = 1;
       break;
     case 3:
-      coinName = "TRXUSDT";
+      coinName = "DOGEUSDT";
       num = 3;
       fix = 5;
       bbfix = 0;
